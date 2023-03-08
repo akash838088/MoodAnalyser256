072e9d67e9483e17fff53fe45be2ea1f5a7aa98b
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-03-09 02:19:39 +0530

    Add UC1-TC1.2-SadReturnSad

--- MoodAnalyser/MoodAnalyserTestCase/UnitTest1.cs
+++ MoodAnalyser/MoodAnalyserTestCase/UnitTest1.cs
@@ -11,7 +11,12 @@ namespace MoodAnalyserTestCase
             string result = mood.AnalizeMood("I am in Sad Mood");
             Assert.AreEqual("Sad", result);
         }
-
+        [Test]
+        public void GivenHappyMood_WhenAnalized_ShouldReturnHappy()
+        {
+            string result = mood.AnalizeMood("I am in Happy Mood");
+            Assert.AreEqual("Happy", result);
+        }
 
     }
 }
