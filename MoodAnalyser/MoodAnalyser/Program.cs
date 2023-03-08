30a712f58e1c0961d9234093c29065a222dc3ef4
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-03-09 01:53:10 +0530

    Add UC1-SadReturnSad

---
+++ MoodAnalyser/MoodAnalyser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MoodAnalyserClass mood = new MoodAnalyserClass();
+            mood.AnalizeMood("I am Sad");
+            Console.ReadLine();
+        }
+    }
+}
+
