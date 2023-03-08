fd209dca994170b0b0beb0aca0c4209d4df3d466
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-03-09 02:38:49 +0530

    Add UC2-TC2.1-NullException

--- MoodAnalyser/MoodAnalyserTestCase/UnitTest1.cs
+++ MoodAnalyser/MoodAnalyserTestCase/UnitTest1.cs
@@ -3,19 +3,28 @@ namespace MoodAnalyserTestCase
 {
     public class Tests
     {
-        MoodAnalyserClass mood = new MoodAnalyserClass("I am in Sad Mood");
+
 
         [Test]
         public void GivenSadMood_WhenAnalized_ShouldReturnSad()
         {
+            MoodAnalyserClass mood = new MoodAnalyserClass("I am in Sad Mood");
             string result = mood.AnalizeMood();
             Assert.AreEqual("Sad", result);
         }
         [Test]
         public void GivenHappyMood_WhenAnalized_ShouldReturnHappy()
         {
+            MoodAnalyserClass mood = new MoodAnalyserClass("I am in Happy Mood");
             string result = mood.AnalizeMood();
-            Assert.AreEqual("Sad", result);
+            Assert.AreEqual("Happy", result);
+        }
+        [Test]
+        public void GivenNullMood_WhenAnalized_ShouldReturnHappy()
+        {
+            MoodAnalyserClass mood = new MoodAnalyserClass(null);
+            string result = mood.AnalizeMood();
+            Assert.AreEqual("Happy", result);
         }
 
     }
